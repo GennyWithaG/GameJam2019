cca4446601e1bc751766636d9193f4931e8a3947
Eric Barkuloo <jbarkuloo@ufl.edu>
2019-01-26 17:00:11 -0500

    Moving alien char
    Char who can move left/right

---
+++ GameJam/Assets/Scripts/alien_Move2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alien_Move2 : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-moveSpeed, 0, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+        }
+    }
+}
